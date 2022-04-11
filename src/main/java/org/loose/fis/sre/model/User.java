e7742bc66ca95f0ab3d4697f6124e504d19ebc03
Alex Kosa <alexkosa@Alexs-MacBook-Pro.local>
2022-04-12 01:14:32 +0300

    User registration database and interface

---
+++ src/main/java/org/loose/fis/sre/model/User.java
@@ -0,0 +1,113 @@
+package org.loose.fis.sre.model;
+
+import org.dizitart.no2.objects.Id;
+
+public class User {
+    @Id
+    private String fullName;
+    private String phoneNumber;
+
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String fullName,String phoneNumber,String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.fullName=fullName;
+        this.phoneNumber=phoneNumber;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (fullName == null) {
+            if (other.fullName != null)
+                return false;
+        } else if (!fullName.equals(other.fullName))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        if (phoneNumber == null) {
+            if (other.phoneNumber != null)
+                return false;
+        } else if (!phoneNumber.equals(other.phoneNumber))
+            return false;
+        if (role == null) {
+            if (other.role != null)
+                return false;
+        } else if (!role.equals(other.role))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        return result;
+    }
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "fullName='" + fullName + '\''+ "phoneNumber='" + phoneNumber + '\'' + '\n';
+    }
+}
