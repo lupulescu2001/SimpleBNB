4f036d4fc9afdfb28c4a0636d6ed7cfa14c18fb7
lupulescu2001 <lupulescu2001@gmail.com>
2022-05-08 14:14:07 +0300

    Leave Review

--- src/main/java/org/loose/fis/sre/controllers/OpenPastClientsController.java
+++ src/main/java/org/loose/fis/sre/controllers/OpenPastClientsController.java
@@ -2,14 +2,23 @@ package org.loose.fis.sre.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import org.loose.fis.sre.services.BookingRequestService;
 
 public class OpenPastClientsController {
     private String username;
     @FXML
     private ListView<String> listView;
+    @FXML
+    private TextField clientUsername;
+    @FXML
+    private TextField clientMark;
     public void setUsername(String username) {
         this.username = username;
         listView.getItems().addAll(BookingRequestService.getPastClients(username));
     }
+    public void handleLeaveReviewAction() {
+
+    }
+
 }
